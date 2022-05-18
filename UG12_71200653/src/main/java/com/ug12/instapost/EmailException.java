b7e7b202adb01c067e4d543d0586e47358d55049
davindipawijaya <79068988+davindipawijaya@users.noreply.github.com>
2022-05-18 18:20:41 +0700

    Add files via upload

---
+++ UG12_71200653/src/main/java/com/ug12/instapost/EmailException.java
@@ -0,0 +1,10 @@
+package com.ug12.instapost;
+
+public class EmailException {
+    private String errorMessage;
+    private int errorCode;
+
+    public String getErrorMessage() {
+
+    }
+}
---
+++ UG12_71200653/src/main/java/com/ug12/instapost/InstaPost.java
@@ -0,0 +1,21 @@
+package com.ug12.instapost;
+
+public class InstaPost {
+    private int totalMention;
+    private String email;
+    private String username;
+
+    public void printInfo() {
+        System.out.println(totalMention);
+        System.out.println(email);
+        System.out.println(username);
+    }
+    public void login(String username) {
+        username =
+        if email=
+    }
+    public void post(String) {
+
+    }
+
+}
