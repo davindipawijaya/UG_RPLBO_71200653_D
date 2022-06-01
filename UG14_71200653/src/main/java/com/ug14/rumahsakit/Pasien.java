6d84b5423e11719f5afdc432d777897eb7db17db
davindipawijaya <79068988+davindipawijaya@users.noreply.github.com>
2022-06-01 18:51:28 +0700

    Add files via upload

---
+++ UG14_71200653/src/main/java/com/ug14/rumahsakit/Pasien.java
@@ -0,0 +1,24 @@
+package com.ug14.rumahsakit;
+
+public class Pasien {
+    private String nama;
+    private int usia;
+    private String alamat;
+    private String penyakit;
+    private int levelPenyakit;
+    private String status;
+
+    public Pasien(String test, int i, String di_mana_saja) {
+
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    public int getLevelPenyakit() {
+        return levelPenyakit;
+    }
+    public String getStatus() {
+        return status;
+    }
+}
---
+++ UG14_71200653/src/main/java/com/ug14/rumahsakit/Suster.java
@@ -0,0 +1,13 @@
+package com.ug14.rumahsakit;
+
+public class Suster {
+    private String nama;
+
+    public Suster(String siti) {
+
+    }
+
+    public void screening(Pasien pasien, Jadwal jadwal) {
+
+    }
+}
