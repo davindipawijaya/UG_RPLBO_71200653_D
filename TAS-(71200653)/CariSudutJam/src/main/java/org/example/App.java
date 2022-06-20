13f0336f9b3ca9c73fbaaef7f9b4c2e567aeeb4a
davindipawijaya <79068988+davindipawijaya@users.noreply.github.com>
2022-06-20 17:52:21 +0700

    Add files via upload

---
+++ TAS-(71200653)/CariSudutJam/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import static org.example.CreateDB.createNewDatabase;
+import static org.example.CreateTable.createNewTable;
+
+public class App {
+    public static void main( String[] args ) {
+        createNewDatabase("waktu.db");
+
+        createNewTable();
+
+        InsertRecords app = new InsertRecords();
+        app.insert("15:00");
+        app.insert("07:30");
+        app.insert("08:05");
+        app.insert("17:35");
+        app.insert("11:01");
+    }
+}
